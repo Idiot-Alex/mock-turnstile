d1034fe1c03201fc1f1837620915ed18753a300a
Eleven <idiotalex@163.com>
2024-12-16 15:25:29 +0800

    chore: add ble permission

--- android-app/app/src/main/java/com/hotstrip/metro/MainActivity.kt
+++ android-app/app/src/main/java/com/hotstrip/metro/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.hotstrip.metro
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -29,12 +33,48 @@ class MainActivity : ComponentActivity() {
         if (isGranted) {
             // 权限被授予
             Toast.makeText(this, "相机权限已授予", Toast.LENGTH_SHORT).show()
+            openCamera()
         } else {
             // 权限被拒绝
             Toast.makeText(this, "相机权限被拒绝", Toast.LENGTH_SHORT).show()
         }
     }
 
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (cameraIntent.resolveActivity(packageManager) != null) {
+            startActivity(cameraIntent)
+        } else {
+            Toast.makeText(this, "没有可用的相机应用", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy {
+        val bluetoothManager = getSystemService(BluetoothManager::class.java)
+        bluetoothManager.adapter
+    }
+
+    private fun startBluetoothScan() {
+        if (bluetoothAdapter != null && bluetoothAdapter!!.isEnabled) {
+            // 开始蓝牙扫描
+            bluetoothAdapter!!.startDiscovery()
+            Toast.makeText(this, "开始扫描蓝牙设备", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "蓝牙未启用", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val requestBluetoothPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (isGranted) {
+            // 权限被授予
+            Toast.makeText(this, "蓝牙权限已授予", Toast.LENGTH_SHORT).show()
+            // 在这里可以添加蓝牙相关的操作
+            startBluetoothScan()
+        } else {
+            // 权限被拒绝
+            Toast.makeText(this, "蓝牙权限被拒绝", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     @Composable
     fun MyApp() {
@@ -62,6 +102,11 @@ class MainActivity : ComponentActivity() {
                 requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
             }) {
                 Text(text = "Click Here")
+            }
+            Button(onClick = {
+                requestBluetoothPermissionLauncher.launch(Manifest.permission.BLUETOOTH)
+            }) {
+                Text(text = "请求蓝牙权限")
             }
         }
     }
